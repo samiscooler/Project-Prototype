5cecd424fd3ce449ba6174e171391a3cd022ee8f
herohamp <hampmoe@me.com>
2017-04-02 22:19:14 +0000

    levelManager

--- classes/levelLoader.class.js
+++ classes/levelLoader.class.js
@@ -7,6 +7,15 @@ function levelLoaderClass(levels) {
         this.currentLevelID = id;
     };
 
+    this.place = function() {
+        if (gameData.mouse != null) {
+            var cords = [(Math.floor(gameData.mouse.x / 50) * 50) / 50, (Math.floor(gameData.mouse.y / 50) * 50) / 50];
+            if (levelManager.currentLevel[cords[1]][cords[0]] == 0) {
+                levelManager.currentLevel[cords[1]][cords[0]] = 1
+            }
+        }
+    }
+
     this.renderCurrentLevel = function() {
         var loc = [0, 0];
         var diff = 50;
--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var ctx = c.getContext("2d");
 var gameData = {
     width: c.width,
     height: c.height,
-    fps: 30
+    fps: 30,
+    mouse: null
 }
 
 var levelManager = new levelLoaderClass(levels);
@@ -17,3 +18,19 @@ setInterval(function() {
 
     levelManager.renderCurrentLevel();
 }, 1000 / gameData.fps);
+
+function getMousePos(c, evt) {
+    var rect = c.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+c.addEventListener('mousemove', function(evt) {
+    gameData.mouse = getMousePos(c, evt)
+}, false);
+
+c.addEventListener('click', function(evt) {
+    levelManager.place();
+}, false);
